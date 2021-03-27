da5c3a39a84bb2c941fc0d00c139af8ba50b8e05
FatCatCoder <christian75armand@gmail.com>
2021-03-27 14:33:27 -0400

    start up, added bootstrap5 to react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h6 class="display-5">edit it like this? Or more like this?</h6>
         <a
           className="App-link"
           href="https://reactjs.org"
